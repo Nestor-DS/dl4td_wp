7d2ecfaae774ffe7c9dd9d9ec88e3c2d936691a0
Nestor-DS <nestorduhamel18@outlook.es>
2024-05-19 05:40:49 -0600

    adding new methods

--- SuperTML/modules/data_preparation.py
+++ SuperTML/modules/data_preparation.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+import seaborn as sns
 import numpy as np
 
 def show_column_position(dat):
@@ -42,7 +43,8 @@ def show_column_position_another(dat):
     draw.text((160, 160), str(dat[0][:11]), fill='white', font=font)
     rgb = [np.array(image, dtype='uint8') for _ in range(3)]
     return np.array(rgb)
-    
+
+# Inserción de datos en imagen 
 def data_to_image(data):
     """
     Convierte datos en imágenes.
@@ -74,6 +76,37 @@ def data_to_image(data):
         rgb = [np.array(image, dtype='uint8') for _ in range(3)]
         data_images.append(rgb)
     return np.array(data_images) / 255
+
+# Barra de grises
+def data_to_heatmap_bw(data):
+    data_heatmaps = []
+
+    # Normalizar min-max
+    min_values = np.min(data, axis=0)
+    max_values = np.max(data, axis=0)
+    min_max_normalized = (data - min_values) / (max_values - min_values)
+
+    # Crear un colormap personalizado en escala de grises
+    cmap = sns.color_palette("Greys", as_cmap=True)
+
+    for row in min_max_normalized:
+        # Obtener el número de características
+        num_features = len(row)
+
+        # Crear un mapa de calor utilizando Seaborn
+        ax = sns.heatmap(data=np.expand_dims(row, axis=0), cmap=cmap, cbar=False, square=True)
+        ax.set_xticks(np.arange(num_features) + 0.5, minor=False)
+        ax.set_xticklabels(np.arange(1, num_features + 1), minor=False)
+        ax.set_yticks([])  # Desactivar las etiquetas de los ejes y
+        heatmap_image = np.array(ax.get_figure().canvas.renderer.buffer_rgba())
+
+        # Ajustar el orden de los grupos de colores
+        if len(data_heatmaps) % 2 == 1:
+            heatmap_image = heatmap_image[:, ::-1, :]
+
+        data_heatmaps.append(heatmap_image)
+
+    return np.array(data_heatmaps) / 255
 
 def data_to_image_another(data):
     """
